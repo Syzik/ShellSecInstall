4fb1fcbeb4d1682e0609bab97ae6c7d962d72da0
syzik <camus.florian@gmail.com>
2019-06-24 17:43:30 +0200

    cme the real one

--- shellSecInstall.sh
+++ shellSecInstall.sh
@@ -9,7 +9,7 @@
 
 #--------------------------------------------------------------
 
-listTools=( "" "install:All:installAll" "git:Nishang:https://github.com/samratashok/nishang" "git:Impacket:https://github.com/SecureAuthCorp/impacket.git" "git:ImpacketStaticBinary:https://github.com/ropnop/impacket_static_binaries.git" "install:Lazagne:installLazagne" "git:CrackMapExec:https://github.com/byt3bl33d3r/CrackMapExec.git" "git:Mimikatz:https://github.com/gentilkiwi/mimikatz.git" "git:ASRPRoast:https://github.com/HarmJ0y/ASREPRoast.git" "git:Rpivot:https://github.com/klsecservices/rpivot" "git:Rubeus:https://github.com/GhostPack/Rubeus.git" "git:SharpHound:https://github.com/BloodHoundAD/SharpHound.git" "install:BloodHound:installBloodHound" "install:Kekeo:installKekeo" "install:Kerbrute:installKerbrute" "apt:Sshuttle:sshuttle" "git:LinEnum:https://github.com/rebootuser/LinEnum.git" "git:PayloadAllTheThings:https://github.com/swisskyrepo/PayloadsAllTheThings.git" "git:Nullinux:https://github.com/m8r0wn/nullinux.git" "git:Sn1per:https://github.com/1N3/Sn1per.git" "git:SecLists:https://github.com/danielmiessler/SecLists.git" "git:Grouper2:https://github.com/l0ss/Grouper2.git" "git:GpoCheck:https://github.com/aurel26/gpocheck.git")
+listTools=( "" "install:All:installAll" "git:Nishang:https://github.com/samratashok/nishang" "git:Impacket:https://github.com/SecureAuthCorp/impacket.git" "git:ImpacketStaticBinary:https://github.com/ropnop/impacket_static_binaries.git" "install:Lazagne:installLazagne" "git:CrackMapExec:https://github.com/byt3bl33d3r/CrackMapExec.git" "git:Mimikatz:https://github.com/gentilkiwi/mimikatz.git" "git:ASRPRoast:https://github.com/HarmJ0y/ASREPRoast.git" "git:Rpivot:https://github.com/klsecservices/rpivot" "git:Rubeus:https://github.com/GhostPack/Rubeus.git" "git:SharpHound:https://github.com/BloodHoundAD/SharpHound.git" "install:BloodHound:installBloodHound" "install:Kekeo:installKekeo" "install:Kerbrute:installKerbrute" "apt:Sshuttle:sshuttle" "git:LinEnum:https://github.com/rebootuser/LinEnum.git" "git:PayloadAllTheThings:https://github.com/swisskyrepo/PayloadsAllTheThings.git" "git:Nullinux:https://github.com/m8r0wn/nullinux.git" "git:Sn1per:https://github.com/1N3/Sn1per.git" "git:SecLists:https://github.com/danielmiessler/SecLists.git" "git:Grouper2:https://github.com/l0ss/Grouper2.git" "git:GpoCheck:https://github.com/aurel26/gpocheck.git" "install:cme:installCme" )
 
 orange='\e[0;33m'
 neutre='\e[0;m'
@@ -125,6 +125,18 @@ installBloodHound(){
     unzip BloodHound-linux-x64.zip
     rm BloodHound-linux-x64.zip
     git clone https://github.com/fox-it/BloodHound.py.git
+}
+
+# CME
+installCme(){
+cd /opt/
+apt-get install -y libssl-dev libffi-dev python-dev build-essential
+pip install --user pipenv
+git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
+cd CrackMapExec && pipenv install
+pipenv shell
+python setup.py install
+cd /opt/
 }
 
 # Kekeo
